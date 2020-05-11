45c4cbe7f873097765be94219eeaf3bb62c480a1
Yuxin Wang <yuxinwang.dev@gmail.com>
2020-05-10 23:50:00 -0400

    Add extra prng parameters to built-in algorithms.

--- statdp/algorithms.py
+++ statdp/algorithms.py
@@ -29,43 +29,43 @@ def _hamming_distance(result1, result2):
     return sum(res1 != res2 for res1, res2 in zip_longest(result1, result2))
 
 
-def noisy_max_v1a(queries, epsilon):
+def noisy_max_v1a(prng, queries, epsilon):
     # find the largest noisy element and return its index
-    return (np.asarray(queries, dtype=np.float64) + np.random.laplace(scale=2.0 / epsilon, size=len(queries))).argmax()
+    return (np.asarray(queries, dtype=np.float64) + prng.laplace(scale=2.0 / epsilon, size=len(queries))).argmax()
 
 
-def noisy_max_v1b(queries, epsilon):
+def noisy_max_v1b(prng, queries, epsilon):
     # INCORRECT: returning maximum value instead of the index
-    return (np.asarray(queries, dtype=np.float64) + np.random.laplace(scale=2.0 / epsilon, size=len(queries))).max()
+    return (np.asarray(queries, dtype=np.float64) + prng.laplace(scale=2.0 / epsilon, size=len(queries))).max()
 
 
-def noisy_max_v2a(queries, epsilon):
-    return (np.asarray(queries, dtype=np.float64) + np.random.exponential(scale=2.0 / epsilon, size=len(queries))).argmax()
+def noisy_max_v2a(prng, queries, epsilon):
+    return (np.asarray(queries, dtype=np.float64) + prng.exponential(scale=2.0 / epsilon, size=len(queries))).argmax()
 
 
-def noisy_max_v2b(queries, epsilon):
+def noisy_max_v2b(prng, queries, epsilon):
     # INCORRECT: returning the maximum value instead of the index
-    return (np.asarray(queries, dtype=np.float64) + np.random.exponential(scale=2.0 / epsilon, size=len(queries))).max()
+    return (np.asarray(queries, dtype=np.float64) + prng.exponential(scale=2.0 / epsilon, size=len(queries))).max()
 
 
-def histogram_eps(queries, epsilon):
+def histogram_eps(prng, queries, epsilon):
     # INCORRECT: using (epsilon) noise instead of (1 / epsilon)
-    noisy_array = np.asarray(queries, dtype=np.float64) + np.random.laplace(scale=epsilon, size=len(queries))
+    noisy_array = np.asarray(queries, dtype=np.float64) + prng.laplace(scale=epsilon, size=len(queries))
     return noisy_array[0]
 
 
-def histogram(queries, epsilon):
-    noisy_array = np.asarray(queries, dtype=np.float64) + np.random.laplace(scale=1.0 / epsilon, size=len(queries))
+def histogram(prng, queries, epsilon):
+    noisy_array = np.asarray(queries, dtype=np.float64) + prng.laplace(scale=1.0 / epsilon, size=len(queries))
     return noisy_array[0]
 
 
-def SVT(queries, epsilon, N, T):
+def SVT(prng, queries, epsilon, N, T):
     out = []
-    eta1 = np.random.laplace(scale=2.0 / epsilon)
+    eta1 = prng.laplace(scale=2.0 / epsilon)
     noisy_T = T + eta1
     c1 = 0
     for query in queries:
-        eta2 = np.random.laplace(scale=4.0 * N / epsilon)
+        eta2 = prng.laplace(scale=4.0 * N / epsilon)
         if query + eta2 >= noisy_T:
             out.append(True)
             c1 += 1
@@ -76,9 +76,9 @@ def SVT(queries, epsilon, N, T):
     return out.count(False)
 
 
-def iSVT1(queries, epsilon, N, T):
+def iSVT1(prng, queries, epsilon, N, T):
     out = []
-    eta1 = np.random.laplace(scale=2.0 / epsilon)
+    eta1 = prng.laplace(scale=2.0 / epsilon)
     noisy_T = T + eta1
     for query in queries:
         # INCORRECT: no noise added to the queries
@@ -92,13 +92,13 @@ def iSVT1(queries, epsilon, N, T):
     return _hamming_distance((True if i < true_count else False for i in range(len(queries))), out)
 
 
-def iSVT2(queries, epsilon, N, T):
+def iSVT2(prng, queries, epsilon, N, T):
     out = []
-    eta1 = np.random.laplace(scale=2.0 / epsilon)
+    eta1 = prng.laplace(scale=2.0 / epsilon)
     noisy_T = T + eta1
     for query in queries:
         # INCORRECT: noise added to queries doesn't scale with N
-        eta2 = np.random.laplace(scale=2.0 / epsilon)
+        eta2 = prng.laplace(scale=2.0 / epsilon)
         if (query + eta2) >= noisy_T:
             out.append(True)
             # INCORRECT: no bounds on the True's to output
@@ -109,14 +109,14 @@ def iSVT2(queries, epsilon, N, T):
     return _hamming_distance((True if i < true_count else False for i in range(len(queries))), out)
 
 
-def iSVT3(queries, epsilon, N, T):
+def iSVT3(prng, queries, epsilon, N, T):
     out = []
-    eta1 = np.random.laplace(scale=4.0 / epsilon)
+    eta1 = prng.laplace(scale=4.0 / epsilon)
     noisy_T = T + eta1
     c1 = 0
     for query in queries:
         # INCORRECT: noise added to queries doesn't scale with N
-        eta2 = np.random.laplace(scale=4.0 / (3.0 * epsilon))
+        eta2 = prng.laplace(scale=4.0 / (3.0 * epsilon))
         if query + eta2 > noisy_T:
             out.append(True)
             c1 += 1
@@ -129,13 +129,13 @@ def iSVT3(queries, epsilon, N, T):
     return _hamming_distance((True if i < true_count else False for i in range(len(queries))), out)
 
 
-def iSVT4(queries, epsilon, N, T):
+def iSVT4(prng, queries, epsilon, N, T):
     out = []
-    eta1 = np.random.laplace(scale=2.0 / epsilon)
+    eta1 = prng.laplace(scale=2.0 / epsilon)
     noisy_T = T + eta1
     c1 = 0
     for query in queries:
-        eta2 = np.random.laplace(scale=2.0 * N / epsilon)
+        eta2 = prng.laplace(scale=2.0 * N / epsilon)
         if query + eta2 > noisy_T:
             # INCORRECT: Output the noisy query instead of True
             out.append(query + eta2)
